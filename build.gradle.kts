1894c23db094642d57ea63e91851af7b8e2c55d8
曾鑫 <573908667@qq.com>
2026-06-10 11:45:45 +0800

    Fix PhpStorm target for runIde

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,14 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.3.5")
+        phpstorm("2025.3.5")
         testFramework(TestFrameworkType.Platform)
 
         // Add plugin dependencies for compilation here:
-        bundledPlugin("com.intellij.java")
-        bundledPlugin("org.jetbrains.kotlin")
         bundledPlugin("JavaScript")
         bundledPlugin("com.intellij.modules.json")
         bundledPlugin("org.intellij.plugins.markdown")
         bundledPlugin("com.intellij.database")
-        compatiblePlugin("com.jetbrains.php")
+        bundledPlugin("com.jetbrains.php")
     }
 }
